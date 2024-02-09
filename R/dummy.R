f4f979ce38fb086d40da5f1fc685d8061ba3150d
Ben Taft <ben.taft@career2cradle.org>
2024-02-09 12:22:49 -0600

    initial setup

---
+++ R/dummy.R
@@ -0,0 +1,11 @@
+#' A dummy function
+#'
+#' Uses [base::cat()] to output "dummy" to the console.
+#' 
+#' @export
+#'
+#' @examples
+#' dummy()
+dummy <- function(){
+    cat("dummy")
+}
---
+++ tests/testthat/test-dummy.R
@@ -0,0 +1,4 @@
+test_that("dummy prints", {
+    expect_output(dummy(),
+                  "dummy")
+})
