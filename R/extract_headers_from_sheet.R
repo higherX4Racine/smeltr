618d6343cb4b196b3c37a12c696e36ebb3420d99
Ben Taft <ben.taft@career2cradle.org>
2024-05-20 12:43:46 -0500

    refactored header extraction and updated documentation

--- R/extract_headers_from_sheet.R
+++ R/extract_headers_from_sheet.R
@@ -1,3 +1,15 @@
+#' Read header information from one sheet in a workbook
+#'
+#' @param .spreadsheet the full path to the workbook file
+#' @param .sheet the name or number of the sheet to parse
+#' @param .nrows the number of rows to investigate as headers
+#'
+#' @return a tibble with 1 + `.nrows` columns.
+#' \describe{
+#' \item{Column}{<int> which column the value came from}
+#' \item{Row\[i\]}{<chr> the header from row `i` in this Column.}
+#' }
+#' @export
 extract_headers_from_sheet <- function(.spreadsheet,
                                        .sheet,
                                        .nrows) {
--- R/extract_headers_from_spreadsheet.R
+++ R/extract_headers_from_spreadsheet.R
@@ -1,3 +1,14 @@
+#' Pull header information from every sheet in a workbook
+#'
+#' @param .spreadsheet the full path to the workbook file
+#' @param .nrows the number of rows to read as headers
+#'
+#' @return a tibble with 2 + `.nrows` columns
+#' * **Sheet** <chr> the name of the sheet the headers come from
+#' * **Column** <int> which column the headers come from
+#' * **Row\[i\]** <chr> the value of the header in row $i$ of the column
+#' @export
+#' @seealso [extract_headers_from_sheet()]
 extract_headers_from_spreadsheet <- function(.spreadsheet,
                                              .nrows) {
     .sheets <- .spreadsheet |>
